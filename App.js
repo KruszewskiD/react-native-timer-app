63ab75bbca27f390cf17892ad6724a1612712f6e
Daniel Kruszewski <danielkruszewski@macbook-air-daniel.home>
2023-06-27 21:26:09 +0200

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import MainScreen from "./Screens/MainScreen";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SafeAreaView style={styles.container}>
+        <MainScreen style={{ flex: 1 }}></MainScreen>
+      </SafeAreaView>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
